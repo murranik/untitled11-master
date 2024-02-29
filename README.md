79b6c0213ffe56934ee1dd4d833c0bd1436fc590
murranik <murranik@gmail.com>
2024-02-29 21:47:59 +0200

    code refactor

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-test
+    public double getWeight() {
+        return weight;
+    } - видалено так як не потрібно
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    } - видалено так як не потрібно
+
+    public double getHeight() {
+        return height;
+    } - видалено так як не потрібно
+
+    public void setHeight(double height) {
+        this.height = height;
+    } - видалено так як не потрібно
+    
+![image](https://github.com/murranik/untitled11-master/assets/60602487/76564789-cf5f-4ddb-9867-7fa9954ab0bb)
+Код конвеншн джави
--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,17 @@
-
-
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        humanIMB humanIMB = new humanIMB(80,1.52);
-        System.out.println(humanIMB.Result());
+        System.out.println(HumanBMI.getResult(80, 1.52));
     }
 }
-class humanIMB {
-    public double W; //Weight Human
-    public double H; // Height Human
-    private static double imb;
-    public humanIMB(double w, double h) {
-        W = w;
-        H = h;
-        imb = W / (H * H);
-    }
-    public double takeW() {
-        return W;
-    }
-    public void putW(double w) {
-        W = w;
-        imb = W / (H * H);
-    }
-    public double takeH() {
-        return H;
-    }
-    public void putH(double h) {
-        H = h;
-        imb = W / (H * H);
-    }
-    public static double takeImt() {
-        return imb;
-    }
-    public static String Result() {
-        String  string = null;
-        if (imb >=18.5 & imb <25) {
-            string ="Norm";
-        }
-        if (imb >=25 & imb <30) {
-            string ="Warning! ";
-        }
-        if (imb >=30) {
-            string ="Fat";
-        }
-        if (imb <18.5) {
-            string ="Deficit";
-        }
-        return string;
+
+class HumanBMI {
+    public static String getResult(double weight, double height) {
+        double resFactor = weight / (height * height);
+        
+        if (resFactor < 18.5) return "Deficit";
+        if (resFactor >= 18.5 && resFactor < 25) return "Norm";
+        if (resFactor >= 25 && resFactor < 30) return "Warning!";
+
+        return "Fat";
     }
 }
